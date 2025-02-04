138a81ff72fcf688b14bb4dbe46e9cd91283e554
Christos Dalamagkas <cdalamagkas@gmail.com>
2025-02-04 15:41:14 +0200

    Revert "feature request #12, django channel messages for user updates"
    This reverts commit b4190ce9cbedbfc053ea035c7af066ba0848c628.

--- users/models.py
+++ users/models.py
@@ -19,9 +19,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
-
-class PasswordReset(models.Model):
-    email = models.EmailField()
-    token = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
